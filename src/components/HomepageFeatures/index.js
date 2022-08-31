425757dfa68f8b7ccccc45e13c0b44f76f8aa833
hari-testpress <hari@testpress.in>
2022-08-31 11:07:30 +0530

    docs: Update home page

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -5,7 +5,7 @@ import styles from './styles.module.css';
 const FeatureList = [
   {
     title: 'Video DRM Encryption',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    Svg: require('@site/static/img/security-svgrepo-com.svg').default,
     description: (
       <>
         No downloader, plugin or hack can download the videos streamed through Testpress.
@@ -14,7 +14,7 @@ const FeatureList = [
   },
   {
     title: 'Dynamic Watermarking',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    Svg: require('@site/static/img/web-security-svgrepo-com.svg').default,
     description: (
       <>
         Total Screen Capture Block in Mobile Apps. Discourage screen capture in websites with user based watermark like IP address, user id, email id, phone no. etc.
@@ -23,7 +23,7 @@ const FeatureList = [
   },
   {
     title: 'Upload & Embed on Your Site Or App',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    Svg: require('@site/static/img/app-plugin-plug-addon-connect-svgrepo-com.svg').default,
     description: (
       <>
         Embed player with subtitles, multiple qualities & speed change options.
